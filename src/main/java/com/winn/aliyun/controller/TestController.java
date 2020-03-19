a067b25b0fe18a5aa10576b58f1d2f105919d4f2
1316084833@qq.com <li1316084833>
2020-03-19 17:32:33 +0800

    日志

--- src/main/java/com/winn/aliyun/controller/TestController.java
+++ src/main/java/com/winn/aliyun/controller/TestController.java
@@ -1,14 +1,29 @@
 package com.winn.aliyun.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class TestController {
+    private static Logger log = LogManager.getLogger(TestController.class);
 
     @RequestMapping("/index")
+    @ResponseBody
     public String index() throws Exception {
         return "阿里云项目启动成功";
     }
+
+    @RequestMapping("/hello")
+    public String hello() throws Exception {
+        log.info("info..............");
+        log.debug("debug..............");
+        log.error("error..............");
+        log.warn("warn..............");
+        return "jsp/hello";
+    }
+
 
 }
