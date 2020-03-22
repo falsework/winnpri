d17c913032c741a3815c5dc3ac6100d3415d762d
alwaysalong <1316084833@qq.com>
2020-03-22 23:27:15 +0800

    整合log4j2-配置日志异步输出的两种方式

--- src/main/java/com/winn/aliyun/AppStart.java
+++ src/main/java/com/winn/aliyun/AppStart.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppStart {
 
     public static void main(String[] args) {
+        //log4j日志异步输出
+        //或者在启动命令中加入java -jar -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector xxx.jar
+        System.setProperty("Log4jContextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
         SpringApplication.run(AppStart.class, args);
     }
 }
