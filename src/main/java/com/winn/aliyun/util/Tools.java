374e75e06aa99813c73cc0472d543041b235b3fd
stride_forward <stride_forward>
2020-06-12 15:51:15 +0800

    代码中常用工具，以及一些代码注释

--- src/main/java/com/winn/aliyun/util/Tools.java
+++ src/main/java/com/winn/aliyun/util/Tools.java
@@ -4,6 +4,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
@@ -19,7 +21,36 @@ public class Tools {
         return str.substring(0, 12);
     }
 
-    public static void main(String[] args) {
-        System.out.println(getRandomUUID12());
+    /**
+     * Description: 对象转string
+     *
+     * @Date: 2020/6/12
+     */
+    public static String ts(Object obj) {
+        String str = "";
+        if (null != obj) {
+            str = String.valueOf(obj).trim();
+        }
+        return str;
+    }
+
+    /**
+     * Description: list等长拆分
+     *
+     * @param list     原list集合
+     * @param pageSize 子集合长度
+     * @Date: 2020/6/12
+     */
+    public static <T> List<List<T>> groupList(List<T> list, int pageSize) {
+        List<List<T>> listGroup = new ArrayList<List<T>>();
+        int listSize = list.size();
+        for (int i = 0; i < listSize; i += pageSize) {
+            if (i + pageSize > listSize) {
+                pageSize = listSize - i;
+            }
+            List<T> newList = list.subList(i, i + pageSize);
+            listGroup.add(newList);
+        }
+        return listGroup;
     }
 }
--- src/main/java/com/winn/aliyun/util/thread/ThreadPoolExecutorUtil.java
+++ src/main/java/com/winn/aliyun/util/thread/ThreadPoolExecutorUtil.java
@@ -29,6 +29,18 @@ public class ThreadPoolExecutorUtil extends ThreadPoolExecutor {
     //如果设置为0，则是不等待，直接退出
     private static final int KEEP_ALIVE_TIME = 30;
 
+    /*
+     * AbortPolicy：该策略是线程池的默认策略。使用该策略时，如果线程池队列满了丢掉这个任务并且抛出RejectedExecutionException异常。
+     *
+     * DiscardPolicy：这个策略和AbortPolicy的slient版本，如果线程池队列满了，会直接丢掉这个任务并且不会有任何异常。
+     *
+     * DiscardOldestPolicy：这个策略从字面上也很好理解，丢弃最老的。也就是说如果队列满了，会将最早进入队列的任务删掉腾出空间，再尝试加入队列。因为队列是队尾进，队头出，所以队头元素是最老的，因此每次都是移除对头元素后再尝试入队。
+     *
+     * CallerRunsPolicy：使用此策略，如果添加到线程池失败，那么主线程会自己去执行该任务，不会等待线程池中的线程去执行。就像是个急脾气的人，我等不到别人来做这件事就干脆自己干。
+     *
+     * 自定义：如果以上策略都不符合业务场景，那么可以自己定义一个拒绝策略，只要实现RejectedExecutionHandler接口，并且实现rejectedExecution方法就可以了。具体的逻辑就在rejectedExecution方法里去定义就OK了。
+     *
+     */
     static {
         //固定线程池
         pool = new ThreadPoolExecutorUtil(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS,
